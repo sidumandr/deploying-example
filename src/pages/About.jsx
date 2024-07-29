97359608cee2049eb8f8adec094c654cb9d34ca9
sidumandr <dumansafailhan@gmail.com>
2024-07-29 19:33:47 +0300

    tittle

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 function About() {
   return (
     <div>
+      <h1>Example About</h1>
       <h1>About</h1> <hr />
       <Link style={{ marginRight: "12px" }} to="employee">
         Çalışanlarımız Hakkında
